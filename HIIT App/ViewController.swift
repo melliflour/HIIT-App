5c33e76aa5bc2954b862ce62aae98ab477776ad7
Faith Lim <melliflour@gmail.com>
2019-09-11 10:27:03 +0800

    Initial Commit

---
+++ HIIT App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HIIT App
+//
+//  Created by Faith on 11/9/19.
+//  Copyright © 2019 Faith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
